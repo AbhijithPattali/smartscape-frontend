77a2a84d23fd102c40e662c0f761f6cd2663d8a8
Abhijith Pattali <ap2092@hw.ac.uk>
2025-01-28 00:32:42 +0400

    Add main application structure with Navbar, Device Management, and Card components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import App from '../app/devices/app';
-=======
 import App from '../app/profiles/app';
->>>>>>> Stashed changes
 
 export default function Home() {
   return (
